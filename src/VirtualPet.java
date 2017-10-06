68f5494db0241b453125589282ffc8755ce819f6
MZiegler <mwziegler2014@gmail.com>
2017-10-06 11:41:31 -0400

    initial code in VirtualPet class, App class has menu loop

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,92 @@
 
 public class VirtualPet {
 
+	// attributes
+	private String name = "Lucy the therapy dog";
+	private int cupsKibbleNeeded = 3;
+	private int cupsWaterNeeded = 3;
+	private int nbrMilesNeedsToWalk = 5;
+
+	private String favToy;
+
+	// hunger level: too full = 0, not hungry = 1; hungry =2
+	private int hungerLevel;
+	// thirst level: not thirsty = 0, thirsty =1
+	private int thirstLevel;
+	private int tiredLevel;
+	private boolean needsToPee;
+	private boolean needsToPoop;
+	private int weight = 50;
+
+	// constructor
+	public VirtualPet() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getHungerLevel() {
+		return hungerLevel;
+	}
+	
+	public int getThirstLevel() {
+		return thirstLevel;
+	}
+	
+	public int getTiredLevel() {
+		return tiredLevel;
+	}
+	
+	void giveFood(int cupsKibble) {
+
+		if (cupsKibble > cupsKibbleNeeded) {
+			weight += 1;
+			hungerLevel = 0;
+		} else if (cupsKibble < cupsKibbleNeeded) {
+			weight -= 1;
+			hungerLevel = 1;
+		} else {
+			hungerLevel = 0;
+		}
+		needsToPoop = true;
+
+	}
+
+	void giveWater(int cupsWater) {
+
+		if (cupsWater >= cupsWaterNeeded) {
+			thirstLevel = 0;
+		} else {
+			thirstLevel = 1;
+		}
+
+		needsToPee = true;
+	}
+
+	void visitPatients() {
+
+	}
+
+	boolean catchFrisbee() {
+		return true;
+	}
+
+	void goForWalk(int nbrMiles) {
+		if (nbrMiles > nbrMilesNeedsToWalk) {
+			tiredLevel = 3;
+		} else if (nbrMiles < nbrMilesNeedsToWalk) {
+			tiredLevel = 2;
+		} else {
+			tiredLevel = 0; // not tired
+
+		}
+
+	}
+
+	String getStatus() {
+		String status = "Lucy is fine";
+		return status;
+	}
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		VirtualPet myPet = new VirtualPet();
+		Scanner input = new Scanner(System.in);
+		
+		
+		String userAction = "1";
+		do { 
+			System.out.println(myPet.getName());
+			System.out.println("Hunger Level: " + myPet.getHungerLevel());
+			System.out.println("Thirst Level: " + myPet.getThirstLevel());
+			System.out.println("Tiredness Level: " + myPet.getTiredLevel());
+			System.out.println("\nPress 1 to feed Lucy");
+			System.out.println("Press 2 to give Lucy water");
+			System.out.println("Press 3 to take Lucy for a walk");
+			System.out.println("Press 4 to quit");
+			
+			userAction = input.nextLine();
+			
+			
+		}
+		while (!userAction.equals("4"));
+				
+			
 	}
 
 }
